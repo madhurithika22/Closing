39727ed0778512e8d0f421ce47875bf352321448
Madhu Rithika <madhurithika22@gmail.com>
2026-06-13 11:02:44 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -30,9 +30,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
         "https://closing-ecru.vercel.app"
     ], 
     allow_credentials=True,
